5ff6db4eba6a418a6a5db539ac91a11d2dac0cca
hakelimopu <hakelimopu@gmail.com>
2016-09-24 15:27:40 -0500

    jetlag

---
+++ JetLag/Deploy/scripts/sounds.js
@@ -0,0 +1,21 @@
+﻿var sounds = {
+    table: { "turn": "content/sounds/turn.wav", "death": "content/sounds/death.wav" },
+    sounds: {},
+    loadSounds: function () {
+        for (var key in this.table) {
+            var sound = document.createElement("audio");
+            sound.src = this.table[key];
+            sound.setAttribute("preload", "auto");
+            sound.setAttribute("controls", "none");
+            sound.display = "none";
+            document.body.appendChild(sound);
+            this.sounds[key] = sound;
+        }
+    },
+    play:function(key){
+        if (this.enabled) {
+            this.sounds[key].play();
+        }
+    },
+    enabled:true
+};
---
+++ JetLag/JetLag/scripts/main.js
@@ -0,0 +1,17 @@
+﻿function resizeCanvas() {
+    frameBuffer.fitToWindow();
+    frameBuffer.present();
+}
+
+function handleKeyDown(event) {
+    mainController.handleKeyDown(event);
+}
+
+function main() {
+    sounds.loadSounds();
+    frameBuffer.initialize(document.getElementById("mainCanvas"), 320, 240);
+    frameBuffer.fitToWindow();
+
+    mainController.context = frameBuffer.backBufferContext;
+    mainController.initialize();
+}
