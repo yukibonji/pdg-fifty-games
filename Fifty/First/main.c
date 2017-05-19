dfc3db655e64adec7e2c93b075850696c4d71c3b
hakelimopu <hakelimopu@gmail.com>
2017-05-19 11:36:21 -0500

    boilerplate

--- Fifty/First/main.c
+++ Fifty/First/main.c
@@ -1,37 +1,45 @@
 #include <SDL.h>
 #include <stdio.h>
 
+#define INIT_FLAGS SDL_INIT_EVERYTHING
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+#define WINDOW_FLAGS 0
 
-int main(int argc, char** argv)
+int Run(Uint32 initFlags, int windowWidth,int windowheight, Uint32 windowFlags, void* (*contextFactory)(), void (*renderFunc)(SDL_Renderer*, const void*), void (*handleEvent)(const SDL_Event*, void*))
 {
-	int result = SDL_Init(SDL_INIT_EVERYTHING);
-	if (result)
-	{
-		printf("Init Error: %s", SDL_GetError());
-		return -1;
-	}
+	SDL_Init(initFlags);
 
-	SDL_Window* window = 0;
-	SDL_Renderer* renderer = 0;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 
-	result = SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
-	if (result)
-	{
-		char* c = SDL_GetError();
-		printf("Init Error: %s", SDL_GetError());
-		return -1;
-	}
+	SDL_CreateWindowAndRenderer(windowWidth, windowheight, windowFlags, &window, &renderer);
 
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-	SDL_Event event;
-	while (SDL_WaitEvent(&event))
+	void* context = contextFactory ? contextFactory() : 0;
+	SDL_Event event = { 0 };
+	for (;;)
 	{
-		if (event.type == SDL_QUIT)
+		SDL_RenderClear(renderer);
+		if (renderFunc)
+		{
+			renderFunc(renderer, context);
+		}
+		SDL_RenderPresent(renderer);
+		if (SDL_WaitEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				break;
+			}
+			else if(handleEvent)
+			{
+				handleEvent(&event, context);
+			}
+		}
+		else
 		{
 			break;
 		}
-		SDL_RenderPresent(renderer);
 	}
 
 	if (renderer)
@@ -49,4 +57,24 @@ int main(int argc, char** argv)
 	SDL_Quit();
 
 	return 0;
+}
+
+void* ContextFactory()
+{
+	return 0;
+}
+
+void RenderFunc(SDL_Renderer* renderer, const void* context)
+{
+
+}
+
+void HandleEvent(const SDL_Event* event, void* context)
+{
+
+}
+
+int main(int argc, char** argv)
+{
+	return Run(INIT_FLAGS, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_FLAGS, ContextFactory, RenderFunc, HandleEvent);
 }
