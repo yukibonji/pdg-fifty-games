9e7e94047ef130f82708f185ee0f481ecf8a7e17
hakelimopu <hakelimopu@gmail.com>
2017-06-07 11:38:08 -0500

    blah

---
+++ FiftyFS/SDLContext/Patterns.fs
@@ -0,0 +1,44 @@
+﻿module SDLContext.Patterns
+
+type pattern = 
+    byte list
+
+let private getStatus (x:int, y:int) (pat:pattern) : bool =
+    if pat.Length <= y then 
+        false
+    else
+        pat
+        |> List.skip y
+        |> List.head
+        |> (&&&) ((1 <<< x) |> byte)
+        |> (<>) 0uy
+
+
+let internal makePattern (data: string list) : pattern =
+    let makeLine (line:string) : byte =
+        line.ToCharArray()
+        |> Array.fold(
+            fun (acc,flag) c -> ((if c='X' then acc + flag else acc), flag <<< 1)) (0uy,1uy)
+        |> fst
+
+    data
+    |> List.map makeLine
+
+type color = byte * byte * byte * byte
+
+let render (x:int,y:int) (foreground: color option, background: color option) (pattern:pattern) (context:SDLContext.context) : unit =
+    [0..((pattern |> List.length) - 1)]
+    |> List.iter (fun cellY -> 
+        [0..7]
+        |>List.iter (fun cellX -> 
+            if pattern |> getStatus(cellX,cellY) then
+                foreground
+                |> Option.iter(fun c -> 
+                    SDLContext.setDrawColor c context
+                    SDLContext.fillRect (x+cellX,y+cellY,1,1) context)
+            else    
+                background
+                |> Option.iter(fun c -> 
+                    SDLContext.setDrawColor c context
+                    SDLContext.fillRect (x+cellX,y+cellY,1,1) context)))
+    ()
---
+++ FiftyFS/SDLContextSandbox/Program.fs
@@ -0,0 +1,24 @@
+﻿open SDLContext
+
+let rec private sdlRunnerLoop (ctx:SDLContext.context) : unit = 
+    SDLContext.setDrawColor (0uy,0uy,0uy,255uy) ctx
+    SDLContext.clear ctx
+    RomFont.render (0,0) (Some (255uy,0uy,0uy,255uy),None) '@' ctx
+    RomFont.renderString (0,8) (Some (255uy,0uy,0uy,255uy),None) "This is a test." ctx
+    SDLContext.present ctx
+    match SDLContext.waitForKeyPress() with
+    | None | Some 27 -> ()
+    | Some _ -> ctx |> sdlRunnerLoop
+
+let sdlRunner (ctx:SDLContext.context) : unit =
+    ctx
+    |> SDLContext.setLogicalSize (320,240)
+
+    ctx 
+    |> sdlRunnerLoop
+
+[<EntryPoint>]
+let main argv =
+    sdlRunner
+    |>SDLContext.run (640,480) SDLContext.WindowFlags.None
+    0
