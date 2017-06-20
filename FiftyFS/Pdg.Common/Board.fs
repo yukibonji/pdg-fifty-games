287dbfb97b7843964de5c722fbbe51432265b518
hakelimopu <hakelimopu@gmail.com>
2017-06-20 15:27:41 -0500

    common

--- FiftyFS/Pdg.Common/Board.fs
+++ FiftyFS/Pdg.Common/Board.fs
@@ -7,8 +7,61 @@ module Location =
     let add (first:location) (second:location) : location =
         ((first |> fst) + (second |> fst), (first |> snd) + (second |> snd))
 
-type board<'TItem> = Map<location,'TItem>
+type boardCell<'TTerrain,'TItem,'TCreature,'TEffect when 'TEffect:comparison> =
+    {terrain:'TTerrain;
+    items:'TItem list;
+    creature: 'TCreature option;
+    effects:Set<'TEffect>}
 
+module BoardCell =
+    
+    let setTerrain (terrain) (boardCell) =
+        {boardCell with terrain = terrain}
+
+    let addEffect (effect) (boardCell) =
+        {boardCell with effects = boardCell.effects |> Set.add effect}
+
+    let removeEffect (effect) (boardCell) =
+        {boardCell with effects = boardCell.effects |> Set.remove effect}
+
+    let removeEffects (boardCell) =
+        {boardCell with effects = Set.empty}
 
     
+type board<'TTerrain,'TItem,'TCreature,'TEffect when 'TEffect:comparison> = 
+    Map<location,boardCell<'TTerrain,'TItem,'TCreature,'TEffect>>
 
+module Board =
+    
+    let setTerrain (location:location) (terrain:'TTerrain) (board:board<'TTerrain,'TItem,'TCreature,'TEffect>) : board<'TTerrain,'TItem,'TCreature,'TEffect> =
+        board
+        |> Map.add location 
+            (match board.TryFind location with
+            | Some cell -> (cell |> BoardCell.setTerrain terrain)
+            | None -> {terrain=terrain;items=[];creature=None;effects=Set.empty})
+
+    let getTerrain (location:location) (board:board<'TTerrain,'TItem,'TCreature,'TEffect>) : 'TTerrain option =
+        match board |> Map.tryFind location with
+        | None -> None
+        | Some t -> t.terrain |> Some
+
+    let addEffect (effect:'TEffect) (location:location) (board:board<'TTerrain,'TItem,'TCreature,'TEffect>) : bool * board<'TTerrain,'TItem,'TCreature,'TEffect> =
+        match board.TryFind location with
+        | None -> (false, board)
+        | Some cell -> (true, board |> Map.add location (cell |> BoardCell.addEffect effect))
+
+    let removeEffect (effect:'TEffect) (location:location) (board:board<'TTerrain,'TItem,'TCreature,'TEffect>) : bool * board<'TTerrain,'TItem,'TCreature,'TEffect> =
+        match board.TryFind location with
+        | None -> (false, board)
+        | Some cell -> (true, board |> Map.add location (cell |> BoardCell.removeEffect effect))
+
+    let removeEffects (location:location) (board:board<'TTerrain,'TItem,'TCreature,'TEffect>) : bool * board<'TTerrain,'TItem,'TCreature,'TEffect> =
+        match board.TryFind location with
+        | None -> (false, board)
+        | Some cell -> (true, board |> Map.add location (cell |> BoardCell.removeEffects))
+
+    let hasEffect (effect:'TEffect) (location:location) (board:board<'TTerrain,'TItem,'TCreature,'TEffect>) : bool =
+        match board.TryFind location with
+        | None -> false
+        | Some cell -> cell.effects.Contains effect
+        
--- FiftyFS/Pdg.Common/Program.fs
+++ FiftyFS/Pdg.Common/Program.fs
@@ -1,3 +1,57 @@
-﻿[<EntryPoint>]
+﻿
+type TerrainType =
+    | Grass
+    | Tree
+    | Stone
+    | Flint
+    | BerryBush
+
+type TreeDescriptor =
+    {wood:int}
+
+type StoneDescriptor =
+    {stone:int}
+
+type FlintDescriptor =
+    {flint:int}
+
+type BerryBushType =
+    | Crimson
+    | Copper
+    | Gold
+    | Jade
+    | Turqoise
+    | Ruby
+    | Amethyst
+    | Silver
+
+type BerryBushDescriptor =
+    {berryBushType:BerryBushType;
+    berries:int}
+
+type Terrain =
+    | Grass
+    | Tree of TreeDescriptor
+    | Stone of StoneDescriptor
+    | Flint of FlintDescriptor
+    | BerryBush of BerryBushDescriptor
+
+let terrainTypeGenerator =
+    [(TerrainType.Grass,100);
+    (TerrainType.Tree,25);
+    (TerrainType.Stone,10);
+    (TerrainType.Flint,5)]
+    |> Map.ofList
+
+let terrainGenerators =
+    [(TerrainType.Grass,fun (random:System.Random) -> Terrain.Grass);
+    (TerrainType.Tree,fun (random:System.Random) -> Terrain.Tree {wood=10});
+    (TerrainType.Stone,fun (random:System.Random) -> Terrain.Stone {stone=10});
+    (TerrainType.Flint,fun (random:System.Random) -> Terrain.Flint {flint=10})]
+    |> Map.ofList
+
+
+[<EntryPoint>]
 let main argv = 
+    
     0
