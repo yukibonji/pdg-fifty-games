6bb8246733b00b32d7cf777fe1b3b1a2aa73f619
hakelimopu <hakelimopu@gmail.com>
2017-06-19 23:27:48 -0500

    blah

--- FiftyFS/Doorways/Program.fs
+++ FiftyFS/Doorways/Program.fs
@@ -15,15 +15,15 @@ let pickRandomlyFromList (random:System.Random) (l:'TItem list) : 'TItem option 
         |> List.head
         |> Some
 
+let pickRandomCellLocation (random:System.Random) (cellMap:cellMap) : (int*int) option=
+    cellMap
+    |> Map.toList
+    |> List.map fst
+    |> pickRandomlyFromList random
+
 let generateCellMap (random:System.Random) (cellCount:int) : cellMap =
     
     let rec generateCells (random:System.Random) (cellCount:int) (cellMap:cellMap) : cellMap =
-
-        let pickRandomCellLocation (random:System.Random) (cellMap:cellMap) : (int*int) option=
-            cellMap
-            |> Map.toList
-            |> List.map fst
-            |> pickRandomlyFromList random
 
         if cellCount<=0 then
             cellMap        
@@ -55,8 +55,69 @@ let generateCellMap (random:System.Random) (cellCount:int) : cellMap =
                     |> Map.add (choice.Value |> snd) (endCell)
                     |> generateCells random (cellCount-1)
 
-    Map.empty
-    |> generateCells random cellCount
+    let cellMap = 
+        Map.empty
+        |> generateCells random cellCount
+
+    let offsetX, offsetY =
+        ((0,0),cellMap)
+        ||> Map.fold (fun (x,y) (kx,ky) _ -> 
+            ((if x < kx then x else kx),(if y < ky then y else ky)))
+
+    (Map.empty,cellMap)
+    ||> Map.fold (fun acc (x,y) v -> 
+        acc
+        |> Map.add (x-offsetX,y-offsetY) v)
+
+type gameState = 
+    {cellMap:cellMap;
+    position:int*int}
+
+let run (gameState:gameState) : unit = 
+
+    let rec runLoop (show:bool) (gameState:gameState) : unit =
+        let cell = 
+            gameState.cellMap.[gameState.position]
+        if show then
+            printfn ""
+            printfn "You are at (%d,%d)" (gameState.position |> fst) (gameState.position |> snd)
+            if cell.exits.Contains(CardinalDirection.North) then
+                printfn "[N]orth"
+            if cell.exits.Contains(CardinalDirection.East) then
+                printfn "[E]ast"
+            if cell.exits.Contains(CardinalDirection.South) then
+                printfn "[S]outh"
+            if cell.exits.Contains(CardinalDirection.West) then
+                printfn "[W]est"
+        match System.Console.ReadKey(true).Key with
+        | System.ConsoleKey.N -> 
+            if cell.exits.Contains(CardinalDirection.North) then
+                {gameState with position=gameState.position |> CardinalDirection.step 1 CardinalDirection.North} |> runLoop true
+            else            
+                gameState |> runLoop false
+
+        | System.ConsoleKey.E -> 
+            if cell.exits.Contains(CardinalDirection.East) then
+                {gameState with position=gameState.position |> CardinalDirection.step 1 CardinalDirection.East} |> runLoop true
+            else            
+                gameState |> runLoop false
+
+        | System.ConsoleKey.S -> 
+            if cell.exits.Contains(CardinalDirection.South) then
+                {gameState with position=gameState.position |> CardinalDirection.step 1 CardinalDirection.South} |> runLoop true
+            else            
+                gameState |> runLoop false
+
+        | System.ConsoleKey.W -> 
+            if cell.exits.Contains(CardinalDirection.West) then
+                {gameState with position=gameState.position |> CardinalDirection.step 1 CardinalDirection.West} |> runLoop true
+            else            
+                gameState |> runLoop false
+
+        | _ -> gameState |> runLoop false
+
+    gameState
+    |> runLoop true
 
 
 [<EntryPoint>]
@@ -64,4 +125,9 @@ let main argv =
     let random = new System.Random()
     let cellMap = 
         generateCellMap random 20
+    let gameState =
+        {cellMap = cellMap;
+        position = (pickRandomCellLocation random cellMap)|>Option.get}
+    gameState
+    |> run
     0
