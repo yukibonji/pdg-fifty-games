ea93524e89f907f43ae984d85a7895de23380eff
hakelimopu <hakelimopu@gmail.com>
2017-06-19 21:58:39 -0500

    doorways

--- FiftyFS/Doorways/Program.fs
+++ FiftyFS/Doorways/Program.fs
@@ -33,13 +33,27 @@ let generateCellMap (random:System.Random) (cellCount:int) : cellMap =
                 |> Map.add (0,0) {exits=Set.empty}
                 |> generateCells random (cellCount-1) 
             else
-                let cell = 
+                let cellLocation = 
                     pickRandomCellLocation random cellMap
                 let neighborCells = 
                     CardinalDirection.directions
-                    |> List.map (fun d -> CardinalDirection.step 1 d cell.Value)
-                    |> List.filter (fun p -> Map.containsKey p cellMap |> not)
-                cellMap
+                    |> List.map (fun d -> (d,(CardinalDirection.step 1 d cellLocation.Value)))
+                    |> List.filter (fun (d,p) -> (cellMap |> Map.containsKey p |> not))
+                match neighborCells with
+                | [] -> 
+                    cellMap
+                    |> generateCells random cellCount
+                | _ -> 
+                    let choice = 
+                        pickRandomlyFromList random neighborCells
+                    let startCell =
+                        {cellMap.[cellLocation.Value] with exits = cellMap.[cellLocation.Value].exits |> Set.add (choice.Value|> fst)}
+                    let endCell = 
+                        {exits=Set.empty |> Set.add (choice.Value|> fst |> CardinalDirection.opposite)}
+                    cellMap
+                    |> Map.add (cellLocation.Value) (startCell)
+                    |> Map.add (choice.Value |> snd) (endCell)
+                    |> generateCells random (cellCount-1)
 
     Map.empty
     |> generateCells random cellCount
@@ -49,5 +63,5 @@ let generateCellMap (random:System.Random) (cellCount:int) : cellMap =
 let main argv = 
     let random = new System.Random()
     let cellMap = 
-        generateCellMap random 500
+        generateCellMap random 20
     0
